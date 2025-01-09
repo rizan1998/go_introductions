06628d3514236e47ad9b30715b3fb690dd938839
rizan1998 <rizanalfalah@gmail.com>
2025-01-09 11:32:29 +0700

    menambah function dalam sebuah parameter fungsinya sama seperti javascript yaitu menggunakan callback

--- project-pertama/vargo/funcgo.go
+++ project-pertama/vargo/funcgo.go
@@ -79,17 +79,45 @@ func Funcgo() {
 	fmt.Println("original number :", numbers2)
 	fmt.Println("filtered number :", newNumbers2)
 
-
 	var max3 = 3
-    var numbers3 = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
-    var howMany, getNumbers = findMax(numbers3, max3)
-    var theNumbers = getNumbers()
+	var numbers3 = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+	var howMany, getNumbers = findMax(numbers3, max3)
+	var theNumbers = getNumbers()
 
-    fmt.Println("numbers\t:", numbers)
-    fmt.Printf("find \t: %d\n\n", max)
+	fmt.Println("numbers\t:", numbers)
+	fmt.Printf("find \t: %d\n\n", max)
 
-    fmt.Println("found \t:", howMany)    // 9
-    fmt.Println("value \t:", theNumbers) // [2 3 0 3 2 0 2 0 3]
+	fmt.Println("found \t:", howMany)    // 9
+	fmt.Println("value \t:", theNumbers) // [2 3 0 3 2 0 2 0 3]
+
+	var data = []string{"wick", "jason", "ethan"}
+	var dataContainsO = filter(data, func(each string) bool {
+		return strings.Contains(each, "o")
+	})
+	var dataLenght5 = filter(data, func(each string) bool {
+		return len(each) == 5
+	})
+
+	fmt.Println("data asli \t\t:", data)
+	// data asli : [wick jason ethan]
+
+	fmt.Println("filter ada huruf \"o\"\t:", dataContainsO)
+	// filter ada huruf "o" : [jason]
+
+	fmt.Println("filter jumlah huruf \"5\"\t:", dataLenght5)
+	// filter jumlah huruf "5" : [jason ethan]
+}
+
+func filter(data []string, callback func(string) bool) []string {
+	var result []string
+	for _, each := range data {
+		if filtered := callback(each); filtered {
+			result = append(result, each)
+		}
+	}
+	// data mentah
+	fmt.Println("data mentah", data)
+	return result
 }
 
 func printMessage(message string, arr []string) {
